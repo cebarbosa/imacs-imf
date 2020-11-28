551b365db229849b1e1b8d563185753f32571ae6
cebarbosa <kadu.barbosa@gmail.com>
2020-11-27 23:05:57 -0300

    Included convolution of models to match observations.

--- testdata/prepare_CvD.py
+++ testdata/prepare_CvD.py
@@ -4,11 +4,18 @@ import os
 import numpy as np
 from astropy.table import Table, vstack
 from astropy.io import fits
+import astropy.constants as const
 from ppxf import ppxf_util
 from spectres import spectres
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+from scipy.ndimage import gaussian_filter
 
-def prepare_VCJ17(data_dir, wave, output, overwrite=False):
+import context
+from paintbox.utils.broad2res import broad2res
+
+def prepare_VCJ17(data_dir, wave, output, overwrite=False, obsres=100,
+                  wextra=50):
     """ Prepare templates for SSP models from Villaume et al. (2017).
 
         Parameters
@@ -21,28 +28,47 @@ def prepare_VCJ17(data_dir, wave, output, overwrite=False):
         Name of the output file (a multi-extension FITS file)
     overwrite: bool (optional)
         Overwrite the output files if they already exist.
-
+    obsres: float
+        Observed resolution (sigma) in km/s
+    wextra: float
+        Additional wavelength used at both sides of the data to avoid border
+        issues in the data processing.
     """
     if os.path.exists(output) and not overwrite:
         return
-    specs = sorted(os.listdir(data_dir))
+    filenames = sorted(os.listdir(data_dir))
     nimf = 16
     imfs = 0.5 + np.arange(nimf) / 5
     x2s, x1s=  np.stack(np.meshgrid(imfs, imfs)).reshape(2, -1)
     ssps, params = [], []
-    for spec in tqdm(specs, desc="Processing SSP files"):
-        T = float(spec.split("_")[3][1:])
-        Z = float(spec.split("_")[4][1:-8].replace("p", "+").replace(
+    velscale = obsres / 5
+    for fname in tqdm(filenames, desc="Processing SSP files"):
+        T = float(fname.split("_")[3][1:])
+        Z = float(fname.split("_")[4][1:-8].replace("p", "+").replace(
                     "m", "-"))
-        data = np.loadtxt(os.path.join(data_dir, spec))
+        data = np.loadtxt(os.path.join(data_dir, fname))
         w = data[:,0]
+        specs = data[:,1:].T
+        ########################################################################
+        # Trim arrays according to wavelength
+        idx = np.where((w >= wave.min() - wextra) &
+                       (w <= wave.max() + wextra))[0]
+        w = w[idx]
+        specs = specs[:, idx]
+        # Convolve models to match observed resolution
+        if obsres > 100:
+            neww = np.exp(ppxf_util.log_rebin([w[0], w[-1]], np.zeros(10),
+                                velscale=velscale)[1])
+            specs = spectres(neww, w, specs, fill=0, verbose=False).T
+            sigma_diff = np.sqrt(obsres**2 - 100**2) / velscale
+            w = neww
+            specs = gaussian_filter(specs, [sigma_diff, 0])
+        specs = spectres(wave, w, specs.T, verbose=False, fill=0.)
+        ssps.append(specs)
         for i, (x1, x2) in enumerate(zip(x1s, x2s)):
             params.append(Table([[Z], [T], [x1], [x2]],
                                 names=["Z", "Age", "x1", "x2"]))
-            ssp = data[:, i+1]
-            newssp = spectres(wave, w, ssp)
-            ssps.append(newssp)
-    ssps = np.array(ssps)
+    ssps = np.vstack(ssps)
     params = vstack(params)
     hdu1 = fits.PrimaryHDU(ssps)
     hdu1.header["EXTNAME"] = "SSPS"
@@ -130,23 +156,39 @@ def prepare_response_functions(data_dir, wave, outprefix, redo=False):
         hdulist = fits.HDUList([hdu1, hdu2, hdu3])
         hdulist.writeto(output, overwrite=True)
 
-if __name__ == "__main__":
-    # Preparing SSP models
-    w1, w2 = 8000, 13000 # Setting the wavelength window
-    models_dir = "/home/kadu/Dropbox/SPINS/CvD18/" # Directory where models are stored
-    ssps_dir = os.path.join(models_dir, "VCJ_v8")
-
-    # Loading the wavelength dispersion from one of the models
-    wave = np.loadtxt(os.path.join(ssps_dir, os.listdir(ssps_dir)[0]), usecols=(0,))
-    idx = np.where((wave >= w1) & (wave <= w2))[0]
-    wave = wave[idx] # Trimming wavelength range
-    # Defining where the models should be stored
-    outdir = os.path.join(os.getcwd(), "templates")
+def prepare_templates_testdata(zmax=0.05):
+    outdir = os.path.join(context.home_dir, "templates")
     if not os.path.exists(outdir):
         os.mkdir(outdir)
-    output = os.path.join(outdir, "VCJ17_varydoublex.fits")
-    prepare_VCJ17(ssps_dir, wave, output)
-    # Preparing response functions
-    rfs_dir = os.path.join(models_dir, "RFN_v3")
-    outprefix = os.path.join(outdir, "C18_rfs")
-    prepare_response_functions(rfs_dir, wave, outprefix)
+    # Read testdata processed file to obtain wavelength ranges.
+    filename = os.path.join(context.home_dir, "data/testdata/NGC7144_spec.fits")
+    # Directory where models are stored
+    models_dir = "/home/kadu/Dropbox/SPINS/CvD18/"
+    ssps_dir = os.path.join(models_dir, "VCJ_v8")
+    # Loading the wavelength dispersion from one of the models
+    wave = np.loadtxt(os.path.join(ssps_dir, os.listdir(ssps_dir)[0]),
+                      usecols=(0,))
+    target_res = np.array([200, 100]) # Rounding up the ideal resolution
+    velscale = np.ceil(target_res / 2.5) # Same as input spectrum
+    wrange = ["blue", "red"]
+    for i, ext in enumerate([1,2]):
+        table = Table.read(filename, hdu=ext)
+        w1data, w2data = table["wave"].min(), table["wave"].max()
+        w1 = (w1data / (1 + zmax) / 100).astype(int) * 100
+        w2 = (w2data / 100).astype(int) * 100 + 100
+        # Using ppxf to obtain a logarithmic-scaled dispersion
+        outwave = np.exp(ppxf_util.log_rebin([w1, w2], np.zeros(10),
+                                      velscale=velscale[i])[1])
+        # Defining where the models should be stored
+        output = os.path.join(outdir, "VCJ17_varydoublex_imacs_{"
+                                      "}.fits".format(wrange[i]))
+
+        prepare_VCJ17(ssps_dir, outwave, output, obsres=target_res[i])
+        input(404)
+        # Preparing response functions
+        rfs_dir = os.path.join(models_dir, "RFN_v3")
+        outprefix = os.path.join(outdir, "C18_imacs_{}_rfs".format(wrange[i]))
+        prepare_response_functions(rfs_dir, wave, outprefix)
+
+if __name__ == "__main__":
+    prepare_templates_testdata()
