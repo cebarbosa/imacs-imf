66d890951905389e8036d032496c471bfa12fb2e
cebarbosa <kadu.barbosa@gmail.com>
2021-05-19 14:43:54 -0300

    Refactored code.

--- testdata/prepare_data.py
+++ testdata/prepare_data.py
@@ -10,7 +10,7 @@ import astropy.constants as const
 from ppxf import ppxf_util
 from spectres import spectres
 
-from paintbox.utils.broad2res import broad2res
+from paintbox.utils import broad2res, disp2vel
 
 import context
 
@@ -22,43 +22,36 @@ def prepare_spectrum(spec_file, outfile, overwrite=False):
     mask = mask.astype(np.bool).astype(np.int)
     # Interpolating flux / fluxerr
     idx = np.where(mask > 0)[0]
-    f = interp1d(wave[idx], flux[idx], fill_value="extrapolate")
-    flux = f(wave)
-    ferr = interp1d(wave[idx], fluxerr[idx], fill_value="extrapolate")
-    fluxerr = ferr(wave)
+    f_interp = interp1d(wave[idx], flux[idx], fill_value="extrapolate")
+    flux = f_interp(wave)
+    ferr_interp = interp1d(wave[idx], fluxerr[idx], fill_value="extrapolate")
+    fluxerr = ferr_interp(wave)
     # Calculating resolution in FWHM
     c = const.c.to("km/s").value
-    fwhm = res_kms / c * wave * 2.335
-    # Splitting the data to work with different resolutions
-
-    wave_ranges = [[4200, 6680], [8200, 8900]]
-    idxs = [np.where((wave >= w1) & (wave < w2))[0] for w1,w2 in wave_ranges]
-    waves = [wave[idx] for idx in idxs]
-    fluxes = [flux[idx] for idx in idxs]
-    fluxerrs = [fluxerr[idx] for idx in idxs]
-    masks = [mask[idx] for idx in idxs]
-    res_kmss = [res_kms[idx] for idx in idxs]
-    fwhms = [fwhm[idx] for idx in idxs]
+    fwhms = res_kms / c * wave * 2.355
     # Homogeneize the resolution
     target_res = np.array([200, 100]) # Rounding up the ideal resolution
-    velscale = 100
+    velscale = (target_res / 3).astype(np.int)
+    # Splitting the data to work with different resolutions
+    wave_ranges = [[4200, 6680], [8200, 8900]]
     names = ["wave", "flux", "fluxerr", "mask"]
     hdulist = [fits.PrimaryHDU()]
-    for i in range(len(wave_ranges)):
-        target_fwhm = target_res[i] / c * waves[i] * 2.355
-        flux, fluxerr = broad2res(waves[i], fluxes[i], fwhms[i], target_fwhm,
-                                  fluxerr=fluxerrs[i])
-        # Using ppxf to obtain a logarithmic-scaled dispersion
-        logwave = ppxf_util.log_rebin([waves[i][0], waves[i][-1]], flux,
-                                      velscale=velscale)[1]
-        newwave = np.exp(logwave)
+    for i, (w1, w2) in enumerate(wave_ranges):
+        idx = np.where((wave >= w1) & (wave < w2))[0]
+        w = wave[idx]
+        f = flux[idx]
+        ferr = fluxerr[idx]
+        m = mask[idx]
+        # res = res_kms[idx] # This was used to check a good target_res
+        fwhm = fwhms[idx]
+        target_fwhm = target_res[i] / c * w * 2.355
+        fbroad, fbroaderr = broad2res(w, f, fwhm, target_fwhm, fluxerr=ferr)
         # Resampling data
-        newflux, newfluxerr = spectres(newwave, waves[i], flux,
-                                       spec_errs=fluxerr)
-        newmask = spectres(newwave, waves[i], masks[i]).astype(np.int).astype(
-                                                                        np.bool)
-        obsmask = -1 * (newmask.astype(np.int) - 1)
-        table = Table([newwave, newflux, newfluxerr, obsmask], names=names)
+        owave = disp2vel([w[0], w[-1]], velscale[i])
+        oflux, ofluxerr = spectres(owave, w, fbroad, spec_errs=fbroaderr)
+        omask = spectres(owave, w, m).astype(np.int).astype(np.bool)
+        obsmask = -1 * (omask.astype(np.int) - 1)
+        table = Table([owave, oflux, ofluxerr, obsmask], names=names)
         hdu = fits.BinTableHDU(table)
         hdulist.append(hdu)
     hdulist = fits.HDUList(hdulist)
